0604718f72986641957097e56cda6a210ff65f76
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-02-10 18:18:23 +0700

    pura-pura kerja lagi :v

--- app/src/main/java/com/finder/gofrendi/finder/SettingActivity.java
+++ app/src/main/java/com/finder/gofrendi/finder/SettingActivity.java
@@ -3,6 +3,8 @@ package com.finder.gofrendi.finder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
@@ -22,6 +24,17 @@ public class SettingActivity extends AppCompatActivity {
         String protocol = intent.getStringExtra("protocol");
         String session = intent.getStringExtra("session");
         String radius = intent.getStringExtra("radius");
+    }
+
+    public void buttonSettingSaveClick(View view) {
+        EditText editTextSettingProtocol = (EditText) findViewById(R.id.editText_setting_protocol);
+        EditText editTextSettingServer = (EditText) findViewById(R.id.editText_setting_server);
+        EditText editTextSettingRadius = (EditText) findViewById(R.id.editText_setting_radius);
+        AppBackEnd backEnd = new AppBackEnd(this);
+        backEnd.protocol = String.valueOf(editTextSettingProtocol.getText());
+        backEnd.server = String.valueOf(editTextSettingServer.getText());
+        backEnd.radius = Double.parseDouble(String.valueOf(editTextSettingRadius.getText()));
+        backEnd.save();
     }
 
 }
